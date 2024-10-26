e8020e2906ba60dcf6d05a743f579d2264ff342a
ʜᴀʀɪ <127658855+HariharanTGX@users.noreply.github.com>
2024-10-26 17:44:05 +0530

    fix

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -271,8 +271,9 @@ async def format_filename(file_, user_id, dirpath=None, isMirror=False):
 
     nfile_ = file_
     if prefix:
-        nfile_ = prefix.replace('\s', ' ') + file_
-        prefix = re_sub(r'<.*?>', '', prefix).replace('\s', ' ')
+        nfile_ = prefix + file_
+        #nfile_ = prefix.replace('\s', ' ') + file_
+        prefix = re_sub(r'<.*?>', '', prefix)#.replace('\s', ' ')
         if not file_.startswith(prefix):
             file_ = f"{prefix}{file_}"
 
